573cda037a7a0c36c58de10e382519113cd9bca5
Фадеева Ульяна Александровна ип-517 <kkk@ru>
2025-10-27 12:54:25 +0700

    hlpm

--- src/exercise1.c
+++ src/exercise1.c
@@ -3,7 +3,7 @@
 int main() {
     double numbers[10];
     double sum = 0.0;
-    printf("Введите 10 вещественных чисел:\n");
+
 
     for (int i = 0; i < 10; i++) {
         scanf("%lf", &numbers[i]);
@@ -14,7 +14,7 @@ int main() {
     double average = sum / 10.0;
 
 
-    printf("Среднее арифметическое: %.3f\n", average);
+    printf("%.3f\n", average);
 
     return 0;
 }
